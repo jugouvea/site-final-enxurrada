0f62b2452748f746da2f77eaaf11c9842bd71834
Júlia Gouvêa <91096056+jugouvea@users.noreply.github.com>
2021-12-17 21:59:27 -0300

    Add files via upload

---
+++ sacola.js
@@ -0,0 +1,7 @@
+let saldo = parseInt(prompt("Quanto vc pode gastar na nossa loja?"))
+
+if (saldo >29.90){
+    alert (`Você pode comprar pelo menos uma "podrinha"`);
+} else if (saldo < 29.90)
+    alert (`Infelizmente não da para comprar nenhuma peça`)
+
---
+++ script.js
@@ -0,0 +1,4 @@
+let nome  = prompt("Olá, você poderia nos dizer seu nome?")
+let mensagem = `Olá ${nome}, seja bem vindo a Topázio Boutique! Dê uma olhada nas nossas roupas e faça sua escolha ♡`
+
+alert (mensagem)
